d37eaae0ca7352d476c8b3994c20b8193b7d9c28
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2025-10-03 10:45:45 +0530

    fix: resolve import case sensitivity and update favicon for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Home from './pages/Home'
-import Layout from './components/layout/layout.jsx'
+import Home from "./pages/Home";
+import Layout from "./components/layout/Layout.jsx";
 
 const App = () => {
   return (
-    <div className='min-h-screen flex flex-col justify-between'>
-    <Layout>
-      <Home />
-    </Layout>
+    <div className="min-h-screen flex flex-col justify-between">
+      <Layout>
+        <Home />
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
